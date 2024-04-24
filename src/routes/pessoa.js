34f51eaaa556edddee992fc609c7047e6d07eb0c
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2024-04-24 19:24:30 -0300

    .

--- src/routes/pessoa.js
+++ src/routes/pessoa.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const controllers = new ControllerExercicio()
 
-router.get("/api/pessoas/", controllers.PegarTodos); // erro aqui
+router.get("/api/pessoas/", controllers.PegarTodos);
 router.get("/api/pessoa/:id", controllers.PegarUm);
 router.post("/api/pessoa", controllers.Adicionar);
 router.put("/api/pessoa/:id", controllers.Alterar);
