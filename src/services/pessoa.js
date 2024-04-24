d5c5c0d7607b3fb04e46172d868cc8440ebea44e
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2024-04-24 20:14:48 -0300

    .

--- src/services/pessoa.js
+++ src/services/pessoa.js
@@ -5,7 +5,7 @@ class ServicoExercicio {
 
   async PegarUm(id) {
     if (!id || isNaN(id)) {
-      throw new Error("Favor digite corretamente o id.") 
+      throw new Error("Favor digite corretamente o id.")
     }
     return repositorio.PegarUm(id)
   }
@@ -18,44 +18,44 @@ class ServicoExercicio {
 
     if (!pessoa.nome) {
       throw new Error("Favor preencher o nome.");
-  } else if (!pessoa.email) {
-      throw new Error("Favor preencher o email.");
-  } else if (!pessoa.senha) {
-
-    if (!pessoa) {
-      throw new Error("Favor preencher o pessoa.")
-    } else if (!pessoa.nome) {
-      throw new Error("Favor preencher o nome.")
     } else if (!pessoa.email) {
-      throw new Error("Favor preencher o email.")
+      throw new Error("Favor preencher o email.");
     } else if (!pessoa.senha) {
-      throw new Error("Favor preencher o senha.")
+
+      if (!pessoa) {
+        throw new Error("Favor preencher o pessoa.")
+      } else if (!pessoa.nome) {
+        throw new Error("Favor preencher o nome.")
+      } else if (!pessoa.email) {
+        throw new Error("Favor preencher o email.")
+      } else if (!pessoa.senha) {
+        throw new Error("Favor preencher o senha.")
+      }
+
+      return repositorio.Adicionar(pessoa)
+
     }
 
-    return repositorio.Adicionar(pessoa)
+    // Expressão regular para validar o formato de um e-mail
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  }
+    // Expressão regular para validar a senha
+    // Pelo menos uma letra maiúscula, uma letra minúscula, um número,
+    // um caractere especial e no mínimo 8 caracteres
+    const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-  // Expressão regular para validar o formato de um e-mail
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  // Expressão regular para validar a senha
-  // Pelo menos uma letra maiúscula, uma letra minúscula, um número,
-  // um caractere especial e no mínimo 8 caracteres
-  const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-  // Verificar se o email está em um formato válido
-  if (!emailRegex.test(pessoa.email)) {
+    // Verificar se o email está em um formato válido
+    if (!emailRegex.test(pessoa.email)) {
       throw new Error('Formato de e-mail inválido.');
-  }
+    }
 
-  // Verificar se a senha atende aos critérios de segurança
-  if (!senhaRegex.test(pessoa.senha)) {
+    // Verificar se a senha atende aos critérios de segurança
+    if (!senhaRegex.test(pessoa.senha)) {
       throw new Error('A senha não atende aos critérios de segurança.');
-  }
+    }
 
-  return repositorio.Adicionar(pessoa);
-}
+    return repositorio.Adicionar(pessoa);
+  }
   async Alterar(id, pessoa) {
     if (!id || isNaN(id)) {
       throw new Error("Favor corretamente o id.")
